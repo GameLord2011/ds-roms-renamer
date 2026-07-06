a3cbac7331f738c30133ccf883a5089af71cb494
GameLord2011 <119822417+GameLord2011@users.noreply.github.com>
2026-07-06 15:21:36 -0400

    Cod golfing. v1.0.2

--- src/main.rs
+++ src/main.rs
@@ -17,30 +17,15 @@ static MESSAGE: [u8; include_bytes!("message.txt").len()] = *include_bytes!("mes
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = std::env::args().collect::<Vec<String>>();
     let mut path = String::new();
+
     if args.len() > 1 {
         path = args[1].clone();
     } else {
         println!("Whar is the file list (.txt file containing the paths to the ds roms):");
         stdin().read_line(&mut path).unwrap();
     }
-    if let Some('\n') = path.chars().next_back() {
-        path.pop();
-    }
-    if let Some('\r') = path.chars().next_back() {
-        path.pop();
-    }
-    if let Some('\"') = path.chars().next_back() {
-        path.pop();
-    }
-    if let Some('\"') = path.chars().rev().next_back() {
-        path.remove(0);
-    }
-    if let Some('\'') = path.chars().next_back() {
-        path.pop();
-    }
-    if let Some('\'') = path.chars().rev().next_back() {
-        path.remove(0);
-    }
+
+    path = path.trim_matches(&['\n', '\r', '\'', '\"']).try_into().unwrap();
 
     if path.is_empty() {
         println!("You can't point me to nothing, sorry!");
@@ -49,52 +34,71 @@ fn main() -> std::io::Result<()> {
             "You can't point me to nothing, sorry!",
         ));
     }
+
     let p = Path::new(&path);
+    // I really don't know why a temporary variable is needed here.
     let temp_1 = fs::read_to_string(p).unwrap();
     let paths = temp_1.lines().collect::<Vec<&str>>();
+
     for p in paths {
         let mut path: String = p.to_string();
-        if let Some('\"') = path.chars().next_back() {
-            path.pop();
+        path = path.trim_matches(&['\'', '\"']).try_into().unwrap();
+
+        let oldname ;
+        let mut og_name_idx;
+
+        #[cfg(target_os = "windows")]
+        {
+            og_name_idx = path.rfind("\\").unwrap_or(0);
+            if og_name_idx == 0 {
+                og_name_idx = path.rfind("/").unwrap();
+            }
         }
-        if let Some('\"') = path.chars().rev().next_back() {
-            path.remove(0);
-        }
-        if let Some('\'') = path.chars().next_back() {
-            path.pop();
-        }
-        if let Some('\'') = path.chars().rev().next_back() {
-            path.remove(0);
+        #[cfg(not(target_os = "windows"))]
+        {
+            og_name_idx = new_path.rfind("/").unwrap();
         }
 
+        og_name_idx += 1;
+        oldname = path.get(og_name_idx..path.len()).unwrap();
+
         let rom = fs::read(&path).unwrap();
+        // Offset of banner file relative to rom start plus offset of english title
+        // relative to banner
         let offset =
-            u32::from_le_bytes([rom[0x68], rom[0x69], rom[0x6A], rom[0x6B]]) as usize + 832;
+            u32::from_le_bytes([rom[0x68], rom[0x69], rom[0x6A], rom[0x6B]]) as usize + 832; 
         let mut name = String::from_utf16_lossy(
             &rom[offset..(offset + 256)]
                 .chunks(2)
                 .map(|e| u16::from_le_bytes(e.try_into().unwrap()))
-                .collect::<Vec<u16>>(),
+                .collect::<Vec<u16>>()
         );
+        // Two seperate ones because [TODO: INSERT VALID REASON HERE].
         name = name.replace("\n", " ").replace("\0", "");
         match rom[15] {
-            0x50 => name = format!("{name} EU"),
-            0x35 => name = format!("{name} US"),
-            0x4A => name = format!("{name} JP"),
-            0x55 => name = format!("{name} AU"),
-            0x5B => name = format!("{name} KE"),
-            0x43 => name = format!("{name} CH"),
-            _ => {}
+            // Standard reigon codes from the gameid (EA doesn't respect this 9 / 10
+            // times)
+            0x35 => name = name + "US",
+            0x43 => name = name + "CH",
+            0x4A => name = name + "JP",
+            0x50 => name = name + "EU",
+            0x55 => name = name + "AU",
+            0x5B => name = name + "KE",
+            _ => ()
         }
+
         #[cfg(target_os = "windows")] {
             name = name.replace(&['<', '>', ':', '\"', '/', '\\', '|', '?', '*'], "");
         }
         #[cfg(not(target_os = "windows"))] {
             name = name.replace("/", "");
         }
-        println!("{name}");
+
+        println!("{oldname} -> {name}.nds");
+
         let mut i: usize;
         let mut new_path = path.clone();
+
         #[cfg(target_os = "windows")]
         {
             i = new_path.rfind("\\").unwrap_or(0);
@@ -106,6 +110,7 @@ fn main() -> std::io::Result<()> {
         {
             i = new_path.rfind("/").unwrap();
         }
+
         i += 1;
         loop {
             if i >= new_path.len() {
@@ -113,7 +118,8 @@ fn main() -> std::io::Result<()> {
             }
             new_path.remove(i);
         }
-        new_path = format!("{new_path}{name}.nds");
+
+        new_path = new_path + &name + ".nds";
         match fs::rename(path, new_path) {
             Ok(_) => (),
             Err(err) => {
@@ -125,5 +131,6 @@ fn main() -> std::io::Result<()> {
             }
         }
     }
+
     Ok(())
 }
